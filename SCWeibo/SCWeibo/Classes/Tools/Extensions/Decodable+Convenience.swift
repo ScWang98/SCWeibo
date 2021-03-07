53156553f6b80060b31c00c64ba5740343407eda
scwang <scwang98@qq.com>
2021-03-08 01:18:48 +0800

    完善Decode

--- SCWeibo/SCWeibo/Classes/Tools/Extensions/Decodable+Convenience.swift
+++ SCWeibo/SCWeibo/Classes/Tools/Extensions/Decodable+Convenience.swift
@@ -7,7 +7,6 @@
 
 import Foundation
 
-
 extension Decodable {
     public static var sc: UtilitiesWrapper<Self>.Type {
         return UtilitiesWrapper.self
@@ -16,40 +15,26 @@ extension Decodable {
 
 extension UtilitiesWrapper where Base: Decodable {
     public static func decode(_ param: Dictionary<AnyHashable, Any>) -> Base? {
-        let result = decode(with: param)
-        if let result = result as? Base {
-            return result
-        }
-        return nil
-    }
-    
-    public static func decode(_ param: Array<Dictionary<AnyHashable, Any>>) -> [Base]? {
-        let results = decode(with: param)
-        if let results = results as? [Base] {
-            return results
-        }
-        return nil
-    }
-    
-    private static func decode(with param: ConvenienceDecodable) -> Any? {
         guard let data = self.getJsonData(with: param),
-            let model = try? JSONDecoder().decode(Base.self, from: data) else {
+              let model = try? JSONDecoder().decode(Base.self, from: data) else {
             return nil
         }
         return model
     }
 
+    public static func decode(_ param: Array<Dictionary<AnyHashable, Any>>) -> [Base]? {
+        guard let data = self.getJsonData(with: param),
+              let models = try? JSONDecoder().decode([Base].self, from: data) else {
+            return nil
+        }
+        return models
+    }
+
     private static func getJsonData(with param: Any) -> Data? {
         guard JSONSerialization.isValidJSONObject(param),
-            let data = try? JSONSerialization.data(withJSONObject: param, options: []) else {
+              let data = try? JSONSerialization.data(withJSONObject: param, options: []) else {
             return nil
         }
         return data
     }
 }
-
-public protocol ConvenienceDecodable {}
-
-extension Dictionary: ConvenienceDecodable {}
-
-extension Array: ConvenienceDecodable {}
