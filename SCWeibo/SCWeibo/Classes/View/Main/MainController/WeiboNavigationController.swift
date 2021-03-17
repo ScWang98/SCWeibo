f5bda7ce38c3f3dc017e261f4b694526da663fd0
scwang <scwang98@qq.com>
2021-03-17 16:32:55 +0800

    删除无用文件

---
+++ SCWeibo/SCWeibo/Classes/View/Main/MainController/WeiboNavigationController.swift
@@ -0,0 +1,17 @@
+//
+//  MNNavigationController.swift
+//  SCWeibo
+//
+//  Created by scwang on 2020/3/10.
+//
+
+import UIKit
+
+class WeiboNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
